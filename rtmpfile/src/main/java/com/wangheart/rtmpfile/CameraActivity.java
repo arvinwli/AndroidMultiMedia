4444105c2eebb0b560d01108e4a2c621d48c778a
eric <ericli2wang@gmail.com>
2017-11-24 14:44:06 +0800

    代码整理修改

--- rtmpfile/src/main/java/com/wangheart/rtmpfile/CameraActivity.java
+++ rtmpfile/src/main/java/com/wangheart/rtmpfile/CameraActivity.java
@@ -1,23 +1,18 @@
 package com.wangheart.rtmpfile;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.SurfaceHolder;
-import android.widget.Toast;
+import android.view.View;
 
 import com.wangheart.rtmpfile.ffmpeg.FFmpegHandle;
 import com.wangheart.rtmpfile.utils.LogUtils;
 
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -32,42 +27,46 @@ import java.util.concurrent.Executors;
 
 public class CameraActivity extends Activity implements SurfaceHolder.Callback {
     private MySurfaceView sv;
-    private int screenWidth = 480;
-    private int screenHeight = 320;
+    private final int WIDTH = 480;
+    private final int HEIGHT = 320;
     private SurfaceHolder mHolder;
-    private Camera mCamera;
-    boolean isPreview = false; // 是否在浏览中
-    private String dir = "CameraDemo1";
     private String url = "rtmp://192.168.31.127/live/test";
-    private StreamTask mStreamTask;
     //采集到每帧数据时间
     long previewTime = 0;
-    //开始编码时间
+    //每帧开始编码时间
     long encodeTime = 0;
     //采集数量
     int count = 0;
     //编码数量
     int encodeCount = 0;
+    //采集数据回调
+    private StreamIt mStreamIt;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        File file = new File(Environment.getExternalStorageDirectory(), dir);
-        if (file.exists()) {
-            file.delete();
-        }
-        file.mkdirs();
         init();
     }
 
     private void init() {
-        FFmpegHandle.init(this);
         sv = findViewById(R.id.sv);
+        mStreamIt = new StreamIt();
+        CameraInterface.getInstance().openCamera(1);
+        Camera.Parameters params = CameraInterface.getInstance().getParams();
+        params.setPictureFormat(ImageFormat.NV21);
+        params.setPictureSize(WIDTH, HEIGHT);
+        params.setPreviewSize(WIDTH, HEIGHT);
+        params.setPreviewFpsRange(30000, 30000);
+        List<String> focusModes = params.getSupportedFocusModes();
+        if (focusModes.contains("continuous-video")) {
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+        }
+        CameraInterface.getInstance().resetParams(params);
+        FFmpegHandle.init(this);
         mHolder = sv.getHolder();
         mHolder.addCallback(this);
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "CameraDemo1" + File.separator + "test.flv";
         FFmpegHandle.getInstance().initVideo(url);
     }
 
@@ -75,140 +74,43 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
     protected void onDestroy() {
         super.onDestroy();
         FFmpegHandle.getInstance().close();
+        CameraInterface.getInstance().releaseCamera();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mCamera == null) {
-            mCamera = getCamera();
-            if (mHolder != null) {
-                setStartPreview(mCamera, mHolder);
-            }
+        if (mHolder != null) {
+            CameraInterface.getInstance().startPreview(mHolder, mStreamIt);
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        releaseCamera();
+        CameraInterface.getInstance().stopPreview();
     }
 
-    private Camera getCamera() {
-        Camera camera;
-        try {
-            //打开相机，默认为后置，可以根据摄像头ID来指定打开前置还是后置
-            camera = Camera.open(1);
-            if (camera != null && !isPreview) {
-                try {
-                    Camera.Parameters parameters = camera.getParameters();
-                    //对拍照参数进行设置
-                    for (Camera.Size size : parameters.getSupportedPictureSizes()) {
-                        LogUtils.d(size.width + "  " + size.height);
-                    }
-                    LogUtils.d("============");
-                    for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-                        LogUtils.d(size.width + "  " + size.height);
-                    }
-                    parameters.setPreviewSize(screenWidth, screenHeight); // 设置预览照片的大小
-                    parameters.setPreviewFpsRange(30000, 30000);
-                    parameters.setPictureFormat(ImageFormat.NV21); // 设置图片格式
-                    parameters.setPictureSize(screenWidth, screenHeight); // 设置照片的大小
-                    camera.setParameters(parameters);
-                    //指定使用哪个SurfaceView来显示预览图片
-                    camera.setPreviewDisplay(sv.getHolder()); // 通过SurfaceView显示取景画面
-                    camera.setPreviewCallback(new StreamIt()); // 设置回调的类
-                    camera.startPreview(); // 开始预览
-                    //Camera.takePicture()方法进行拍照
-                    camera.autoFocus(null); // 自动对焦
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                isPreview = true;
-            }
-        } catch (Exception e) {
-            camera = null;
-            e.printStackTrace();
-            Toast.makeText(this, "无法获取前置摄像头", Toast.LENGTH_LONG);
-        }
-        return camera;
-    }
-
-    public static void followScreenOrientation(Context context, Camera camera) {
-        final int orientation = context.getResources().getConfiguration().orientation;
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-        } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            camera.setDisplayOrientation(90);
-        }
-    }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        setStartPreview(mCamera, mHolder);
+        CameraInterface.getInstance().startPreview(mHolder, mStreamIt);
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        setStartPreview(mCamera, mHolder);
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        releaseCamera();
-    }
-
-
-    private void setStartPreview(Camera camera, SurfaceHolder holder) {
-        try {
-
-            camera.setPreviewDisplay(holder);
-            followScreenOrientation(this, camera);
-            camera.startPreview();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private class StreamTask extends AsyncTask<Void, Void, Void> {
-
-        private byte[] mData;
-
-        //构造函数
-        StreamTask(byte[] data) {
-            this.mData = data;
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            // TODO Auto-generated method stub
-            if (mData != null) {
-                encodeTime = System.currentTimeMillis();
-                FFmpegHandle.getInstance().onFrameCallback(mData);
-                LogUtils.d((++count) + "消耗时间:" + (System.currentTimeMillis() - encodeTime));
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-    }
-
-    /*
-    释放相机资源
-     */
-    private void releaseCamera() {
-        if (mCamera != null) {
-            mCamera.setPreviewCallback(null);
-            mCamera.stopPreview();
-            mCamera.release();
-            mCamera = null;
-        }
+        CameraInterface.getInstance().stopPreview();
+        CameraInterface.getInstance().releaseCamera();
     }
 
     ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public void btnStart(View view) {
+    }
 
     public class StreamIt implements Camera.PreviewCallback {
         @Override
---
+++ rtmpfile/src/main/java/com/wangheart/rtmpfile/CameraInterface.java
@@ -0,0 +1,212 @@
+package com.wangheart.rtmpfile;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.view.SurfaceHolder;
+
+import com.wangheart.rtmpfile.utils.FileUtil;
+import com.wangheart.rtmpfile.utils.ImageUtil;
+import com.wangheart.rtmpfile.utils.LogUtils;
+
+import java.io.IOException;
+
+public class CameraInterface {
+    private Camera mCamera;
+    private boolean isPreviewing = false;
+    private static CameraInterface mCameraInterface;
+
+
+
+    private CameraInterface() {
+
+    }
+
+    public static synchronized CameraInterface getInstance() {
+        if (mCameraInterface == null) {
+            mCameraInterface = new CameraInterface();
+        }
+        return mCameraInterface;
+    }
+
+    /**
+     * 打开Camera
+     *
+     */
+    public boolean openCamera(int cameraId) {
+        LogUtils.d("Camera open...");
+        if (cameraId < 0 || cameraId >= Camera.getNumberOfCameras()) {
+            LogUtils.e("cameraId is out of range");
+            return false;
+        }
+        if (mCamera != null) {
+            LogUtils.e("Camera is using...");
+            stopPreview();
+            releaseCamera();
+        }
+        mCamera = Camera.open(cameraId);
+        LogUtils.d("Camera open over....");
+        return true;
+    }
+
+    public Camera.Parameters getParams() {
+        if (mCamera != null)
+            return mCamera.getParameters();
+        else return null;
+    }
+
+    public void resetParams(Camera.Parameters param) {
+        mCamera.setParameters(param);
+    }
+
+    /**
+     * 使用Surfaceview开启预览
+     *
+     * @param holder
+     */
+    public void startPreview(SurfaceHolder holder, Camera.PreviewCallback cb) {
+        LogUtils.d("doStartPreview...");
+        stopPreview();
+        if (mCamera != null) {
+            try {
+                mCamera.setPreviewDisplay(holder);
+                if (cb != null) {
+                    mCamera.setPreviewCallback(cb);
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            preview();
+        }
+    }
+
+
+    public void followScreenOrientation(Context context) {
+        if (mCamera == null)
+            return;
+        final int orientation = context.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mCamera.setDisplayOrientation(90);
+        }
+    }
+
+    /**
+     * 使用TextureView预览Camera
+     *
+     * @param surface
+     */
+    public synchronized void startPreview(SurfaceTexture surface) {
+        LogUtils.d("doStartPreview...");
+        stopPreview();
+        if (mCamera != null) {
+            try {
+                mCamera.setPreviewTexture(surface);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            preview();
+        }
+    }
+
+
+    private synchronized void preview() {
+        mCamera.startPreview();//开启预览
+        isPreviewing = true;
+        Camera.Parameters mParams = mCamera.getParameters(); //重新get一次
+        LogUtils.d("最终设置:PreviewSize--With = " + mParams.getPreviewSize().width
+                + "Height = " + mParams.getPreviewSize().height);
+        LogUtils.d("最终设置:PictureSize--With = " + mParams.getPictureSize().width
+                + "Height = " + mParams.getPictureSize().height);
+    }
+
+
+    public synchronized void stopPreview() {
+        if (mCamera != null && isPreviewing) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            isPreviewing = false;
+        }
+    }
+
+    /**
+     * 停止预览，释放Camera
+     */
+    public synchronized void releaseCamera() {
+        if (null != mCamera) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    /**
+     * 拍照
+     */
+    public void takePicture() {
+        if (isPreviewing && (mCamera != null)) {
+            mCamera.takePicture(mShutterCallback, null, mJpegPictureCallback);
+        }
+    }
+
+    public boolean isPreviewing() {
+        return isPreviewing;
+    }
+
+
+    public Camera getCamera() {
+        return mCamera;
+    }
+
+    /*为了实现拍照的快门声音及拍照保存照片需要下面三个回调变量*/
+    ShutterCallback mShutterCallback = new ShutterCallback()
+            //快门按下的回调，在这里我们可以设置类似播放“咔嚓”声之类的操作。默认的就是咔嚓。
+    {
+        public void onShutter() {
+            // TODO Auto-generated method stub
+            LogUtils.d("myShutterCallback:onShutter...");
+        }
+    };
+    PictureCallback mRawCallback = new PictureCallback()
+            // 拍摄的未压缩原数据的回调,可以为null
+    {
+
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // TODO Auto-generated method stub
+            LogUtils.d("myRawCallback:onPictureTaken...");
+
+        }
+    };
+    PictureCallback mJpegPictureCallback = new PictureCallback()
+            //对jpeg图像数据的回调,最重要的一个回调
+    {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // TODO Auto-generated method stub
+            LogUtils.d("myJpegCallback:onPictureTaken...");
+            Bitmap b = null;
+            if (null != data) {
+                b = BitmapFactory.decodeByteArray(data, 0, data.length);//data是字节数据，将其解析成位图
+                mCamera.stopPreview();
+                isPreviewing = false;
+            }
+            //保存图片到sdcard
+            if (null != b) {
+                //设置FOCUS_MODE_CONTINUOUS_VIDEO)之后，myParam.set("rotation", 90)失效。
+                //图片竟然不能旋转了，故这里要旋转下
+                Bitmap rotaBitmap = ImageUtil.getRotateBitmap(b, 90.0f);
+                FileUtil.saveBitmap(rotaBitmap);
+            }
+            //再次进入预览
+            mCamera.startPreview();
+            isPreviewing = true;
+        }
+    };
+
+
+}
