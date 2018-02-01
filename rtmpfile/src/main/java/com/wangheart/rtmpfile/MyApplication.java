50cd2a4ead7dec75b3ea6d613b496b4c9b0ada56
eric <ericli2wang@gmail.com>
2018-02-01 10:39:38 +0800

    MyApplication

---
+++ rtmpfile/src/main/java/com/wangheart/rtmpfile/MyApplication.java
@@ -0,0 +1,19 @@
+package com.wangheart.rtmpfile;
+
+import android.app.Application;
+
+/**
+ * Author : eric
+ * CreateDate : 2018/2/1  10:21
+ * Email : ericli_wang@163.com
+ * Version : 2.0
+ * Desc :
+ * Modified :
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
